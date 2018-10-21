d97134f8d43500b761b2c40f4b8d0590b2a43c3e
hyphenn <asynck@protonmail.com>
2018-10-21 19:45:27 +0200

    fix(tomcat): context + class scan

--- src/main/java/training/webservice/App.java
+++ src/main/java/training/webservice/App.java
@@ -1,3 +1,5 @@
+package training.webservice;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
--- src/main/java/training/webservice/PingController.java
+++ src/main/java/training/webservice/PingController.java
@@ -1,3 +1,5 @@
+package training.webservice;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
