a4f3327ec6944874a9937e91657f59028717ed60
hyphenn <asynck@protonmail.com>
2018-10-21 18:37:24 +0200

    feat(sample): Spring Boot init project

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+    public static void main(String[] args) { }
+}
